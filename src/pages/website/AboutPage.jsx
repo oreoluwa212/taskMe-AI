b382221ef7853210411f95a39f672168582572bf
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 12:52:48 +0100

    feat: added ABout page

---
+++ src/pages/website/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Fragment } from "react";
+import NavBar from "../../components/website/NavBar";
+import Footer from "../../components/website/Footer";
+
+const AboutPage = () => {
+  return (
+    <Fragment>
+      <NavBar />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default AboutPage;
