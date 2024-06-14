33092329cd073dc3892a0b59408749d2845122ec
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 17:58:35 +0100

    update: updated App.jsx for Login route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import LandingPage from "./pages/website/LandingPage";
 import AboutPage from "./pages/website/AboutPage";
 import FeaturesPage from "./pages/website/FeaturesPage";
+import LoginPage from "./pages/website/LoginPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
