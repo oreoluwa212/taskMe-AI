e4cb9415f4a9745f972281e5100f6eb7af745f71
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 18:32:27 +0100

    update: auth forms started

--- src/pages/website/LoginPage.jsx
+++ src/pages/website/LoginPage.jsx
@@ -1,26 +1,51 @@
-import React from 'react'
-import { loginSignImg, logo } from '../../../public';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { loginSignImg } from "../../../public";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <div className="w-full h-screen flex">
-      <div className="w-[50%] h-full flex overflow-hidden">
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-full md:w-[50%] h-full flex overflow-hidden">
         <img
           src={loginSignImg}
           className="w-full h-full object-cover"
-          alt="Sign Up"
+          alt="Login"
         />
       </div>
-      <div className="w-[50%] h-full relative flex items-start justify-end p-4">
-        <div className="pr-4 pt-3">
-         <Link to={"/"}>
-          <img src={logo} alt="Logo" />
-         </Link>
+      <div className="w-full md:w-[50%] h-full flex flex-col items-center justify-center bg-secondary relative p-4">
+        <Link
+          to="/"
+          className="absolute top-4 right-4 md:text-white text-lg text-secondary md:block hidden"
+        >
+          Home
+        </Link>
+        <div className="bg-white p-8 rounded shadow-lg w-80 mt-16 md:mt-0">
+          <h2 className="text-2xl mb-4 text-center">Login</h2>
+          <form>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <button className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+              Login
+            </button>
+          </form>
         </div>
+        <Link
+          to="/"
+          className="md:hidden absolute bottom-4 right-4 text-lg text-white"
+        >
+          Home
+        </Link>
       </div>
     </div>
   );
-}
+};
 
-export default LoginPage
+export default LoginPage;
--- src/pages/website/SignUpPage.jsx
+++ src/pages/website/SignUpPage.jsx
@@ -1,23 +1,53 @@
 import React from "react";
-import { loginSignImg, logo } from "../../../public";
+import { loginSignImg } from "../../../public";
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
   return (
-    <div className="w-full h-screen flex">
-      <div className="w-[50%] h-full flex overflow-hidden">
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-full md:w-[50%] h-full flex overflow-hidden">
         <img
           src={loginSignImg}
           className="w-full h-full object-cover"
           alt="Sign Up"
         />
       </div>
-      <div className="w-[50%] h-full relative flex items-start justify-end p-4">
-        <div className="pr-4 pt-3">
-          <Link to={"/"}>
-            <img src={logo} alt="Logo" />
-          </Link>
+      <div className="w-full md:w-[50%] h-full flex flex-col items-center justify-center bg-secondary relative p-4">
+        <Link
+          to="/"
+          className="absolute top-4 right-4 md:text-white text-lg text-secondary md:block hidden"
+        >
+          Home
+        </Link>
+        <div className="bg-white p-8 rounded shadow-lg w-80 mt-16 md:mt-0">
+          <h2 className="text-2xl mb-4 text-center">Sign Up</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full p-2 mb-4 border border-gray-300 rounded"
+            />
+            <button className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+              Sign Up
+            </button>
+          </form>
         </div>
+        <Link
+          to="/"
+          className="md:hidden absolute bottom-4 right-4 text-lg text-white"
+        >
+          Home
+        </Link>
       </div>
     </div>
   );
