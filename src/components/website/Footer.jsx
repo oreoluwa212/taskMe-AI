abf2191e740d9f6222704fc848650bccedcd29fc
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 12:32:49 +0100

    fix: fixing footer TaskMe position

--- src/components/website/Footer.jsx
+++ src/components/website/Footer.jsx
@@ -4,11 +4,11 @@ function Footer() {
   return (
     <div className="bg-secondary text-white px-[5%] mt-5 w-full py-10 flex flex-col gap-8">
       <div className="container mx-auto flex lgss:flex-row flex-col justify-between items-start px-6">
-        <div className="flex flex-col gap-8 pt-8">
+        <div className="flex flex-col gap-8 pt-5">
           <h2 className="text-2xl font-semibold mb-2">TaskMe</h2>
           <p>Helping you boost your productivity level.</p>
         </div>
-        <div className="flex lgss:w-[40%] w-full mt-5 justify-between">
+        <div className="flex lgss:w-[40%] w-full mt-10 lgss:mt-0 justify-between">
           <ul className="flex flex-col gap-4">
             <li>Home</li>
             <li>About</li>
