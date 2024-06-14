7cd0e04caf014d9d048c1a6d06dad387f6df4d8c
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 18:22:56 +0100

    feat: started login and signup pages

--- src/pages/website/LoginPage.jsx
+++ src/pages/website/LoginPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { loginSignImg, logo } from '../../../public';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="w-full h-screen flex">
+      <div className="w-[50%] h-full flex overflow-hidden">
+        <img
+          src={loginSignImg}
+          className="w-full h-full object-cover"
+          alt="Sign Up"
+        />
+      </div>
+      <div className="w-[50%] h-full relative flex items-start justify-end p-4">
+        <div className="pr-4 pt-3">
+         <Link to={"/"}>
+          <img src={logo} alt="Logo" />
+         </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage
