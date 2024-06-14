44e6139788ecea0cdc40e13e515a930c9cfec451
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 05:01:18 +0100

    update: hero section updated

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 import logo from '../public/images/TaskMe.png'
 import hero1 from "../public/images/hero1.png";
 import hero2 from "../public/images/hero2.png";
+import hero3 from "../public/images/hero3.svg";
+import heroCircle1 from "../public/images/heroCircle1.png";
+import heroCircle2 from "../public/images/heroCircle2.png";
 
-export { logo, hero1, hero2 };
+export { logo, hero1, hero2, hero3, heroCircle1, heroCircle2 };
--- src/components/website/HeroSection.jsx
+++ src/components/website/HeroSection.jsx
@@ -1,10 +1,10 @@
-import { hero1, hero2 } from "../../../public";
+import { hero1, hero2, hero3, heroCircle1, heroCircle2 } from "../../../public";
 import CustomBtn from "./buttons/CustomBtn";
 
 function HeroSection() {
   return (
     <div className="w-full flex flex-col">
-      <div className="flex lgss:flex-row flex-col">
+      <div className="flex lgss:flex-row flex-col lgss:mb-10">
         <img className="hidden lgss:flex h-[80vh] w-[30%]" src={hero1} alt="" />
         <div className="flex flex-col justify-center items-center text-center mx-auto font-lato gap-6 px-[5%] h-[100vh] lgss:h-auto lgss:px-0">
           <h1 className="text-dark font-bold text-[26px] lgss:text-[40px] text-center flex flex-col">
@@ -23,6 +23,23 @@ function HeroSection() {
           </div>
         </div>
         <img className="hidden lgss:flex h-[80vh] w-[30%]" src={hero2} alt="" />
+      </div>
+      <div className="relative w-full flex justify-center items-center lgss:pt-7 lgss:mt-10">
+        <img
+          className="absolute bottom-[1%] lgss:bottom-[-20%] left-5 lgss:left-[23%] h-[20vh] w-[20vh] lgss:h-[25vh] lgss:w-[25vh]"
+          src={heroCircle1}
+          alt=""
+        />
+        <img
+          className="relative z-10 h-[40vh] w-[45vh] lgss:h-[60vh] lgss:w-[100vh]"
+          src={hero3}
+          alt=""
+        />
+        <img
+          className="absolute top-[1%] lgss:top-[-10%] right-9 lgss:right-[22%] h-[20vh] w-[20vh] lgss:h-[25vh] lgss:w-[25vh]"
+          src={heroCircle2}
+          alt=""
+        />
       </div>
     </div>
   );
