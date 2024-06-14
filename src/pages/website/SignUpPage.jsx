f2a9bfcd0c36d09023887ad21f7c5e8f3dc979ac
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 18:48:31 +0100

    update: auth forms formatted

--- src/pages/website/SignUpPage.jsx
+++ src/pages/website/SignUpPage.jsx
@@ -16,7 +16,9 @@ const SignUpPage = () => {
         <Link to="/" className="absolute top-4 right-4">
           <img src={logo} alt="" />
         </Link>
-        <h2 className="text-2xl font-semibold mb-4 text-center">Create your free account</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center">
+          Create your free account
+        </h2>
         <div className="bg-white p-8 rounded shadow-lg w-80 mt-16 md:mt-0">
           <form>
             <input
