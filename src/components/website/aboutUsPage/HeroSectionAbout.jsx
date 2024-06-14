3ce85079d9b803925ee03653644d46f0da085238
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 13:53:47 +0100

    update: added hero text

--- src/components/website/aboutUsPage/HeroSectionAbout.jsx
+++ src/components/website/aboutUsPage/HeroSectionAbout.jsx
@@ -1,12 +1,17 @@
-import {
-  heroAbout,
-} from "../../../../public";
+import React from "react";
+import { heroAbout } from "../../../../public";
 
 function HeroSectionAbout() {
   return (
-    <div className="w-full flex flex-col">
-      <div className="flex lgss:flex-row flex-col w-full">
-        <img src={heroAbout} alt="" />
+    <div
+      className="w-full flex items-center justify-center bg-cover bg-center bg-no-repeat  h-[80vh]"
+      style={{ backgroundImage: `url(${heroAbout})` }}
+    >
+      <div className="w-full max-w-7xl mx-auto">
+        <h1 className="text-white text-left font-bold text-md md:text-2xl lg:text-4xl w-[90%] lgss:w-[50%]">
+          On a mission to revolutionize how people manage their tasks and
+          projects with the help of AI.
+        </h1>
       </div>
     </div>
   );
