83b6b9f534a89ac975c0f73fa7f2f974cdf72c35
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 18:01:16 +0100

    update: updated NavBar for Signup route

--- src/components/website/buttons/GetStartedBtn.jsx
+++ src/components/website/buttons/GetStartedBtn.jsx
@@ -5,7 +5,7 @@ import { Fragment } from "react";
 function GetStartedBtn() {
   return (
     <Fragment>
-      <Link to={"/"}>
+      <Link to={"/signup"}>
         <button className="lgss:px-5 px-3 py-3 bg-primary text-white capitalize rounded-[8px]">
           create an account
         </button>
