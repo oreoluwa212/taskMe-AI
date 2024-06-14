4385c777d391023ec73de55e29965dd7bd06e394
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 12:25:26 +0100

    fix: fixing footer links unused divs

--- src/components/website/Footer.jsx
+++ src/components/website/Footer.jsx
@@ -9,23 +9,21 @@ function Footer() {
           <p>Helping you boost your productivity level.</p>
         </div>
         <div className="flex lgss:w-[30%] w-full mt-5 justify-between">
-            <ul className="flex flex-col gap-4">
-              <li>Home</li>
-              <li>About</li>
-              <li>Features</li>
-            </ul>
-            <ul className="flex flex-col gap-4">
-              <li>Contact</li>
-              <li>Privacy Policy</li>
-              <li>Terms of Use</li>
-            </ul>
-          <div>
-            <ul className="flex flex-col gap-4">
-              <li>Instagram</li>
-              <li>Twitter</li>
-              <li>LinkedIn</li>
-            </ul>
-          </div>
+          <ul className="flex flex-col gap-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Features</li>
+          </ul>
+          <ul className="flex flex-col gap-4">
+            <li>Contact</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+          </ul>
+          <ul className="flex flex-col gap-4">
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>LinkedIn</li>
+          </ul>
         </div>
       </div>
       <div className="text-center mt-10">
