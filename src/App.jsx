3dd59915253d7407e6cb6588f75c8fde3a0f496e
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 17:59:14 +0100

    update: updated App.jsx for Signup route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/website/LandingPage";
 import AboutPage from "./pages/website/AboutPage";
 import FeaturesPage from "./pages/website/FeaturesPage";
 import LoginPage from "./pages/website/LoginPage";
+import SignUpPage from "./pages/website/SignUpPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
