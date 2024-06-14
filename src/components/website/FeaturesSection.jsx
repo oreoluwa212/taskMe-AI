2377657e29c0b066d6724fae88114c6fa8a53ef7
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 08:56:38 +0100

    update: features content added

--- src/components/website/FeaturesSection.jsx
+++ src/components/website/FeaturesSection.jsx
@@ -1,4 +1,7 @@
 import FeaturesCard from "./cards/FeaturesCard";
+import { GrCopy } from "react-icons/gr";
+import { IoFlash } from "react-icons/io5";
+
 
 function FeaturesSection () {
   return (
@@ -7,13 +10,33 @@ function FeaturesSection () {
         <p>FEATURES</p>
       </div>
       <div className="flex flex-col gap-2">
-      <h2 className="font-semibold text-3xl">Unleash the Power of TaskMe</h2>
-      <p className="font-medium">Dive into the features that make TaskMe a game changer.</p>
+        <h2 className="font-semibold text-3xl">Unleash the Power of TaskMe</h2>
+        <p className="font-medium">
+          Dive into the features that make TaskMe a game changer.
+        </p>
       </div>
       <div className="flex lgss:flex-row flex-col justify-between items-center w-[70%] gap-16 mt-5 py-6">
-         <FeaturesCard/>
-         <FeaturesCard/>
-         <FeaturesCard/>
+        <FeaturesCard
+          title={"Task Breakdown"}
+          description={
+            "Input your project and let TaskMe break it down into manageable daily tasks."
+          }
+          icon={<GrCopy />}
+          className={"text-[#56AC60]"}
+        />
+        <FeaturesCard
+          title={"Progress Tracking"}
+          description={
+            "Track your progress daily and stay on top of your deadlines."
+          }
+        />
+        <FeaturesCard
+          title={"User Friendly Interface"}
+          description={
+            "Enjoy an intuitive and user friendly interface designed for efficiency."
+          }
+          icon={<IoFlash/>}
+        />
       </div>
     </div>
   );
--- src/components/website/cards/FeaturesCard.jsx
+++ src/components/website/cards/FeaturesCard.jsx
@@ -1,7 +1,15 @@
-function FeaturesCard() {
-  return (
-    <div className="bg-white rounded-[8px] shadow-lg h-[200px] w-[80%] lgss:w-1/3 p-3">FeaturesCard</div>
-  )
-}
+import React from "react";
 
-export default FeaturesCard
+const FeaturesCard = ({ title, description, icon, className }) => {
+  return (
+    <div className="bg-white shadow-lg shadow-gray-300/50 h-[280px] max-w-[300px] w-[80%] rounded-[8px] flex flex-col justify-center items-start text-center px-2">
+      {icon && (
+        <div className={className}>{icon}</div>
+      )}
+      <h2 className="text-xl text-red font-bold mb-2">{title}</h2>
+      <p className="text-left text-gray-700">{description}</p>
+    </div>
+  );
+};
+
+export default FeaturesCard;
