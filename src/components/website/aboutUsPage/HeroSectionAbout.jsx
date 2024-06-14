5685e2100ae47c5132e7f0fe36e469896195d854
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 13:59:45 +0100

    fix: made hero text responsive

--- src/components/website/aboutUsPage/HeroSectionAbout.jsx
+++ src/components/website/aboutUsPage/HeroSectionAbout.jsx
@@ -4,11 +4,11 @@ import { heroAbout } from "../../../../public";
 function HeroSectionAbout() {
   return (
     <div
-      className="w-full flex items-center justify-center bg-cover bg-center bg-no-repeat  h-[80vh]"
+      className="w-full flex items-center justify-center bg-cover bg-center bg-no-repeat  h-[90vh]"
       style={{ backgroundImage: `url(${heroAbout})` }}
     >
       <div className="w-full max-w-7xl mx-auto">
-        <h1 className="text-white text-left font-bold text-md md:text-2xl lg:text-4xl w-[90%] lgss:w-[50%]">
+        <h1 className="text-white lgss:text-left text-center font-bold text-2xl lg:text-4xl px-4 lgss:w-[50%]">
           On a mission to revolutionize how people manage their tasks and
           projects with the help of AI.
         </h1>
