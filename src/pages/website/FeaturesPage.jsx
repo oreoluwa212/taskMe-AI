f1ac6616203c9e5cdb8dc57fbae776a9e3af0ae5
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 12:55:46 +0100

    feat: added Features page

---
+++ src/pages/website/FeaturesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Fragment } from "react";
+import NavBar from "../../components/website/NavBar";
+import Footer from "../../components/website/Footer";
+
+const FeaturesPage = () => {
+  return (
+    <Fragment>
+      <NavBar />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default FeaturesPage;
