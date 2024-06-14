eb52107a537e69b15ab0181d9b6832f6be8cc6a3
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 09:49:30 +0100

    update: how it works content added

--- src/components/website/HowItWorks.jsx
+++ src/components/website/HowItWorks.jsx
@@ -1,16 +1,43 @@
-import H1Text from "./headerText/H1Text"
+// components/website/HowItWorks.js
+
+import React from "react";
+import H1Text from "./headerText/H1Text";
 import HeaderBg from "./headerText/HeaderBg";
+import HowItWorksCard from "./cards/HowItWorksCard";
 
 function HowItWorks() {
   return (
-    <div className="bg-white w-full h-[50vh] font-lato flex flex-col py-10 items-center">
+    <div className="bg-white w-full h-fit font-lato flex flex-col py-10 items-center">
       <HeaderBg headerText={"How it works"} />
       <H1Text
         h2Text={"Inside TaskMe's Engine"}
-        pText={"Explore our very seemless process."}
+        pText={"Explore our very seamless process."}
       />
+      <div className="flex justify-around items-start w-full px-[10%] mt-10">
+        <HowItWorksCard
+          step={"Step 1"}
+          h3={"Input Your Project"}
+          p={
+            "Enter the details of your project, including the timeline and the key milestones."
+          }
+        />
+        <HowItWorksCard
+          step={"Step 2"}
+          h3={"Get Daily Tasks"}
+          p={
+            "TaskMe will break down your project into manageable daily tasks,ensuring progress"
+          }
+        />
+        <HowItWorksCard
+          step={"Step 3"}
+          h3={"Track Your Progress"}
+          p={
+            "Monitor your progress, adjust tasks as needed and stay on top of your deadlines."
+          }
+        />
+      </div>
     </div>
   );
 }
 
-export default HowItWorks
+export default HowItWorks;
---
+++ src/components/website/cards/HowItWorksCard.jsx
@@ -0,0 +1,13 @@
+function HowItWorksCard({ step, p, h3}) {
+  return (
+    <div className="flex flex-col items-start px-10 py-5">
+      <div className="text-blue-600 font-semibold mb-8">{step}</div>
+      <h3 className="text-xl font-semibold mb-2">{h3}</h3>
+      <p className="text-gray-700">
+        {p}
+      </p>
+    </div>
+  );
+}
+
+export default HowItWorksCard
