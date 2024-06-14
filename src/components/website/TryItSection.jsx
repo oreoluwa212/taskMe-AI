e62191be60808a58ba8e4fbd7b5607a02ee86ee6
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 12:05:45 +0100

    fix

--- src/components/website/TryItSection.jsx
+++ src/components/website/TryItSection.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import H1Text from "./headerText/H1Text";
+import { bluedot, hero2 } from "../../../public";
 
 function TryItSection() {
   return (
-    <div className="bg-white w-full h-fit font-lato flex flex-col items-center text-center px-5 py-20">
-      <div className="bg-blue-100 text-blue-800 font-semibold px-4 py-2 rounded-full mb-4">
+    <div className="relative bg-white w-full h-fit font-lato flex flex-col items-center text-center px-5 py-20">
+      {/* Blue dot */}
+      <div className="absolute top-5 left-5 rounded-full mt-2 mr-2">
+        <img src={bluedot} alt="" />
+      </div>
+
+      {/* Content */}
+      <div className="bg-blue-100 text-blue-800 font-semibold px-4 py-2 rounded-full my-8">
         Free Trial
       </div>
       <H1Text
@@ -13,9 +20,13 @@ function TryItSection() {
           "Boost your productivity level with TaskMe today and never look back."
         }
       />
-      <button className="bg-primary text-white px-6 mt-10 py-3 rounded-lg font-semibold">
+      <button className="bg-primary text-white px-6 my-14 py-3 rounded-lg font-semibold">
         Start your journey for free
       </button>
+      <div
+        className="absolute inset-0 bg-no-repeat bg-right"
+        style={{ backgroundImage: `url(${hero2})` }}
+      ></div>
     </div>
   );
 }
