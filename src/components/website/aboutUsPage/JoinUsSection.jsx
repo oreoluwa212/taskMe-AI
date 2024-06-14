d7506ab297a3658c9686728fde3f322bad7ed771
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-14 13:09:00 +0100

    feat: added Join Us section to About Page with fixes on width

--- src/components/website/aboutUsPage/JoinUsSection.jsx
+++ src/components/website/aboutUsPage/JoinUsSection.jsx
@@ -6,12 +6,14 @@ function JoinUsSection() {
   return (
     <div className="relative bg-white w-full h-fit font-lato flex flex-col items-center text-center px-5 py-20">
       <HeaderBg headerText={"join us"} />
+      <div className="w-[40%]">
       <H1Text
         h2Text={"Join Us"}
         pText={
           "Whether you're a freelancer juggling multiple projects, a small business owner looking to streamline your workflow, TaskMe is here for you. "
         }
-      />
+        />
+        </div>
       <button className="bg-primary text-white px-6 my-14 py-3 rounded-lg font-semibold">
         Start your journey for free
       </button>
