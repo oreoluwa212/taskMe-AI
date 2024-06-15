a9ee45849ef7686bb8765a21eaa3de968208ad75
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 09:06:56 +0100

    feat: About Page continued, about us section created

--- src/components/website/aboutUsPage/AboutUs.jsx
+++ src/components/website/aboutUsPage/AboutUs.jsx
@@ -3,7 +3,7 @@ import HeaderBg from "../headerText/HeaderBg";
 function AboutUs() {
   return (
     <div className="w-full flex flex-col justify-center items-center h-fit py-20 mb-10">
-      <HeaderBg headerText={"join us"} />
+      <HeaderBg headerText={"about us"} />
     </div>
   );
 }
