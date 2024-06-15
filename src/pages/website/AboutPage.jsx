a71bfb6b56526f893e2d195d64b391ebf4540471
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 09:05:22 +0100

    feat: About Page continued, about us section created

--- src/pages/website/AboutPage.jsx
+++ src/pages/website/AboutPage.jsx
@@ -4,12 +4,14 @@ import NavBar from "../../components/website/NavBar";
 import Footer from "../../components/website/Footer";
 import JoinUsSection from "../../components/website/aboutUsPage/JoinUsSection";
 import HeroSectionAbout from "../../components/website/aboutUsPage/HeroSectionAbout";
+import AboutUs from "../../components/website/aboutUsPage/AboutUs";
 
 const AboutPage = () => {
   return (
     <Fragment>
       <NavBar />
       <HeroSectionAbout/>
+      <AboutUs/>
       <JoinUsSection />
       <Footer />
     </Fragment>
