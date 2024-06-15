6779b46bd1870f1c479432de1e5ad850f21025bb
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 08:45:40 +0100

    fix: mobile responsive features page

--- src/components/website/featuresPage/FeaturesPageCard.jsx
+++ src/components/website/featuresPage/FeaturesPageCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FeaturesPageCard = ({ title, description, imageUrl, className }) => {
   return (
-    <div className="bg-white shadow-lg shadow-gray-300/50 h-fit max-w-[350px] w-[90%] rounded-[8px] flex flex-col justify-center items-start text-center py-10 pl-7 pr-3">
+    <div className="bg-white shadow-custom-xl shadow-gray-300/50 h-fit max-w-[350px] w-[90%] rounded-[8px] flex flex-col justify-center items-start text-center py-10 pl-7 pr-3">
       {imageUrl && (
         <div className={`${className} p-3 rounded-[16px] bg-[#DDEEDF] mb-6`}>
           <img src={imageUrl} alt={title} className="h-10 w-10 object-cover" />
