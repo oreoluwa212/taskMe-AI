259757533b277bdf11b0870e6ebb450c7fdbbb88
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 05:31:25 +0100

    update: verify email modal added

---
+++ src/components/website/modals/VerifyEmailModal.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import H1Text from "../headerText/H1Text";
+
+const VerifyEmailModal = ({ email, onResend, onVerified }) => {
+  return (
+    <div className="w-[35%] bg-white py-4 px-[5%] flex flex-col mx-auto rounded shadow-lg">
+      <H1Text
+        h2Text={"Verify your email address"}
+        pText={`A verification email has been sent to ${email}.`}
+      />
+      <div className="mt-4 flex justify-center">
+        <button
+          onClick={onResend}
+          className="bg-primary text-white px-4 py-2 rounded"
+        >
+          Resend Email
+        </button>
+      </div>
+      <div className="mt-4 flex justify-center">
+        <button
+          onClick={onVerified}
+          className="bg-primary text-white px-4 py-2 rounded"
+        >
+          I've Verified
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyEmailModal;
