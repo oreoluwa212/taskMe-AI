aae1c960a34492f5c1bc2903a6fe12320c4f96ad
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 09:25:05 +0100

    feat: About Page continued, about us section textadded

--- src/components/website/aboutUsPage/AboutUs.jsx
+++ src/components/website/aboutUsPage/AboutUs.jsx
@@ -15,22 +15,26 @@ function AboutUs() {
   return (
     <div className="w-full flex flex-col justify-center items-center h-fit py-20 mb-10">
       <HeaderBg headerText={"about us"} />
-      <div className="w-4/5 lg:w-3/5 text-center">
-        <Section title="Our Story">
-          TaskMe was born out of the collective frustration of a group of
-          passionate individuals who felt overwhelmed by the complexity of task
-          management tools available in the market. We believe there had to be a
-          better way, a simpler, more intuitive solution that could help
-          individuals stay organized, productive and focused on what matters
-          most.
-        </Section>
-        <Section title="Our Approach">
-          We're not just another task management tool, we're your trusted
-          partner in productivity. Our approach is simple yet powerful; we
-          combine cutting-edge technology with intuitive design to create a
-          seamless task management experience. With TaskMe, you can break down
-          projects into manageable tasks and track your progress in real time.
-        </Section>
+      <div className="w-4/5 lg:w-3/5 text-center flex gap-2">
+        <div className="w-1/2">
+          <Section title="Our Story">
+            TaskMe was born out of the collective frustration of a group of
+            passionate individuals who felt overwhelmed by the complexity of
+            task management tools available in the market. We believe there had
+            to be a better way, a simpler, more intuitive solution that could
+            help individuals stay organized, productive and focused on what
+            matters most.
+          </Section>
+          <Section title="Our Approach">
+            We're not just another task management tool, we're your trusted
+            partner in productivity. Our approach is simple yet powerful; we
+            combine cutting-edge technology with intuitive design to create a
+            seamless task management experience. With TaskMe, you can break down
+            projects into manageable tasks and track your progress in real time.
+          </Section>
+        </div>
+        <div className="my-16 w-[2px] rounded-full bg-primary"></div>
+        <div className="w-1/2">
         <Section title="Our Mission">
           Our mission is clear; to empower individuals to achieve their goals
           with ease and efficiency. We believe that everyone deserves access to
@@ -41,6 +45,7 @@ function AboutUs() {
           everything we do. We're committed to providing a product that's not
           only easy to use, but also a joy to use.
         </Section>
+      </div>
       </div>
     </div>
   );
