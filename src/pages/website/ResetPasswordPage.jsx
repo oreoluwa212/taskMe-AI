b3d05bbd88346f2db5d8515dc70cabeb47f0d986
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 05:02:37 +0100

    update: reset password header text added

--- src/pages/website/ResetPasswordPage.jsx
+++ src/pages/website/ResetPasswordPage.jsx
@@ -3,7 +3,6 @@ import EmailModal from "../../components/website/modals/EmailModal";
 import CodeVerificationModal from "../../components/website/modals/CodeVerificationModal";
 import { ResetPasswordBg } from "../../../public";
 
-
 const ResetPasswordPage = () => {
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
@@ -19,10 +18,10 @@ const ResetPasswordPage = () => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center relative">
-      <div className="w-full h-full lgss:flex overflow-hidden absolute top-0 left-0">
+      <div className="w-full lgss:flex overflow-hidden absolute top-0 left-0">
         <img
           src={ResetPasswordBg}
-          className=""
+          className="h-1/2 object-cover"
           alt="Reset Password"
         />
       </div>
