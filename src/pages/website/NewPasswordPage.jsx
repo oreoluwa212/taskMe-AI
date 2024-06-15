ace22789d5bb107e1e7eee8e48e90c533dbd05c1
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 07:52:44 +0100

    fix: mobile responsive OTP code page

--- src/pages/website/NewPasswordPage.jsx
+++ src/pages/website/NewPasswordPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FormComponent from "../../components/website/cards/FormComponent";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { loginSignImg, logo } from "../../../public";
 
 const NewPasswordPage = () => {
+  const navigate = useNavigate();
+
   const fields = [
     { type: "password", label: "New Password" },
     { type: "password", label: "Confirm Password" },
@@ -11,6 +13,8 @@ const NewPasswordPage = () => {
 
   const handleSubmit = (formValues) => {
     console.log(formValues);
+    // After handling form submission, navigate to /login
+    navigate("/login");
   };
 
   return (
@@ -29,7 +33,7 @@ const NewPasswordPage = () => {
         <h2 className="text-lg lgss:text-2xl font-semibold lgss:mb-4 text-center">
           Create New Password
         </h2>
-        <div className="bg-white lgss:w-[60%] px-10 rounded shadow-custom-xl py-5 lgss:mt-4 mt-0">
+        <div className="bg-white lgss:w-[60%] w-[80%] px-10 rounded shadow-custom-xl py-5 lgss:mt-4 mt-0">
           <FormComponent
             fields={fields}
             buttonText="Create new password"
