038baf1d1835d85d2f3495009a4147b66383ab50
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 05:52:49 +0100

    update: verify email modal added

--- src/pages/website/SignUpPage.jsx
+++ src/pages/website/SignUpPage.jsx
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import { loginSignImg, logo } from "../../../public";
 import { Link } from "react-router-dom";
 import FormComponent from "../../components/website/cards/FormComponent";
-import VerifyEmailModal from "../../components/website/modals/VerifyEmailModal";
-import CodeVerificationModal from "../../components/website/modals/CodeVerificationModal";
-import { loginSignImg, logo } from "../../../public";
 
 const SignUpPage = () => {
-  const [step, setStep] = useState(1);
-  const [email, setEmail] = useState("");
-
   const fields = [
     { type: "text", label: "First Name" },
     { type: "text", label: "Last Name" },
@@ -17,28 +12,13 @@ const SignUpPage = () => {
   ];
 
   const handleSubmit = (formValues) => {
-    setEmail(formValues.email);
-    setStep(2);
-    // Here you would normally trigger the email verification process
+    console.log(formValues);
   };
 
-  const handleResend = () => {
-    // Logic to resend the verification email
-    console.log("Resend email to", email);
-  };
-
-  const handleVerified = () => {
-    setStep(3);
-  };
-
-  const handleCodeVerification = () => {
-    console.log("OTP Verified");
-    // Redirect to the welcome page or dashboard
-  };
 
   return (
     <div className="w-full h-screen flex flex-col md:flex-row">
-      <div className="hidden w-full md:w-[50%] h-full lgss:flex overflow-hidden">
+      <div className=" hidden w-full md:w-[50%] h-full lgss:flex overflow-hidden">
         <img
           src={loginSignImg}
           className="w-full h-full object-cover"
@@ -47,40 +27,26 @@ const SignUpPage = () => {
       </div>
       <div className="w-full md:w-[50%] h-full flex flex-col items-center justify-center relative pt-8">
         <Link to="/" className="absolute top-4 right-4 text-white text-lg">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="" />
         </Link>
         <h2 className="text-lg lgss:text-2xl font-semibold lgss:mb-4 text-center">
           Create your free account
         </h2>
         <div className="bg-white lgss:w-[60%] px-10 rounded shadow-custom-xl py-5 lgss:mt-4 mt-0">
-          {step === 1 && (
-            <FormComponent
-              fields={fields}
-              buttonText="Sign Up"
-              onSubmit={handleSubmit}
-            />
-          )}
-          {step === 2 && (
-            <VerifyEmailModal
-              email={email}
-              onResend={handleResend}
-              onVerified={handleVerified}
-            />
-          )}
-          {step === 3 && (
-            <CodeVerificationModal onVerify={handleCodeVerification} />
-          )}
-          {step === 1 && (
-            <div className="mt-4 text-center justify-center flex gap-2">
-              <p className="text-gray-700">Already have an account?</p>
-              <Link
-                to="/login"
-                className="text-primary font-semibold hover:underline"
-              >
-                Log in
-              </Link>
-            </div>
-          )}
+          <FormComponent
+            fields={fields}
+            buttonText="Sign Up"
+            onSubmit={handleSubmit}
+          />
+          <div className="mt-4 text-center justify-center flex gap-2">
+            <p className="text-gray-700">Already have an account?</p>
+            <Link
+              to="/login"
+              className="text-primary font-semibold hover:underline"
+            >
+              Log in
+            </Link>
+          </div>
         </div>
       </div>
     </div>
