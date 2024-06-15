816848e91e4140152f70ae218ebbb05d843b0251
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-15 09:50:13 +0100

    fix

--- src/components/website/cards/FeaturesCard.jsx
+++ src/components/website/cards/FeaturesCard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const FeaturesCard = ({ title, description, imageUrl, className }) => {
   return (
-    <div className="bg-white shadow-lg shadow-gray-300/50 h-fit max-w-[350px] w-[90%] rounded-[8px] flex flex-col justify-center items-start text-center py-10 pl-7 pr-3">
+    <div className="bg-white shadow-lg shadow-gray-300/50 h-fit max-w-[380px] w-[90%] rounded-[8px] flex flex-col justify-center items-start text-center py-10 px-8">
       {imageUrl && (
         <div className={`${className} p-3 rounded-[16px] bg-[#E6F0FF] mb-6`}>
           <img src={imageUrl} alt={title} className="h-10 w-10 object-cover" />
         </div>
       )}
       <h2 className="text-xl text-red font-bold my-3">{title}</h2>
-      <p className="text-left text-gray-700 pt-2 font-medium">{description}</p>
+      <p className="text-left text-gray-700 pt-2 font-semibold">{description}</p>
     </div>
   );
 };
