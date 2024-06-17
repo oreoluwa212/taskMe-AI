d1cbf6934fea2383d2fdcc20a2434e43a342ebe4
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-17 06:43:39 +0100

    feat: sidebar responsive

--- src/components/webApp/Sidebar.jsx
+++ src/components/webApp/Sidebar.jsx
@@ -1,75 +1,132 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { IoIosSettings } from "react-icons/io";
 import { HiOutlineLogout } from "react-icons/hi";
 import { HiClipboardDocumentCheck, HiSquares2X2 } from "react-icons/hi2";
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { logo } from '../../../public';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { logo } from "../../../public";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const [activebutton, setActiveButton] = useState(1);
   useEffect(() => {
     if (location.pathname === "/overview") setActiveButton(1);
     else if (location.pathname === "/projects") setActiveButton(2);
-    else if (
-      location.pathname === "/settings"
-    )
-      setActiveButton(3);
+    else if (location.pathname === "/settings") setActiveButton(3);
   }, [location.pathname]);
 
   return (
-    <div className=" w-1/5 h-screen flex flex-col border-r pb-5 font-lato border-[#19437E] justify-start px-4 items-start pt-8 bg-white">
-      <div className="w-[210px] h-[40px]">
-        <img src={logo} alt="" className="w-full h-full bg-cover" />
-      </div>
-      <div className="mt-10 w-full flex flex-col gap-2 justify-between h-full items-center py-[40%]">
-        <div className="w-full flex flex-col ">
-          <Link
-            to="/overview"
-            className={
-              activebutton === 1
-                ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
-                : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
-            }
-          >
-            <h4 className="">Overview</h4>
-            <HiSquares2X2 />
-          </Link>
-          <Link
-            to="/projects"
-            className={
-              activebutton === 2
-                ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
-                : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
-            }
-          >
-            <h4 className="">Projects</h4>
-            <HiClipboardDocumentCheck />
-          </Link>
-          <Link
-            to="/settings"
-            className={
-              activebutton === 3
-                ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
-                : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
-            }
-          >
-            <h4 className="">Settings</h4>
-            <IoIosSettings />
-          </Link>
+    <>
+      <div className="w-1/5 hidden lgss:flex h-screen flex-col border-r pb-5 font-lato border-[#19437E] justify-start px-4 items-start pt-8 bg-white">
+        <div className="w-[210px] h-[40px]">
+          <img src={logo} alt="" className="w-full h-full bg-cover" />
         </div>
+        <div className="mt-10 w-full flex flex-col gap-2 justify-between h-full items-center py-[40%]">
+          <div className="w-full flex flex-col ">
+            <Link
+              to="/overview"
+              className={
+                activebutton === 1
+                  ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                  : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+              }
+            >
+              <h4 className="">Overview</h4>
+              <HiSquares2X2 />
+            </Link>
+            <Link
+              to="/projects"
+              className={
+                activebutton === 2
+                  ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                  : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+              }
+            >
+              <h4 className="">Projects</h4>
+              <HiClipboardDocumentCheck />
+            </Link>
+            <Link
+              to="/settings"
+              className={
+                activebutton === 3
+                  ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                  : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+              }
+            >
+              <h4 className="">Settings</h4>
+              <IoIosSettings />
+            </Link>
+          </div>
 
-        <button
-          onClick={() => navigate("/login")}
-          className="flex justify-between text-[20px] mt-4 text-[#B82323] items-center  font-medium  px-4  w-full "
-        >
-          Logout
-          <HiOutlineLogout />
-        </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="flex justify-between text-[20px] mt-4 text-[#B82323] items-center  font-medium  px-4  w-full "
+          >
+            Logout
+            <HiOutlineLogout />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
 
-export default Sidebar
+      {isOpen && (
+        <div
+          className={`absolute top-0 left-0  h-screen bg-[#F0EDE7] w-[70%] mds:w-1/2 lgss:hidden flex flex-col px-[5%] pt-10 transform transition-transform duration-300 ${
+            isOpen ? "translate-x-0 " : "-translate-x-full"
+          }`}
+        >
+          <div className="lgss:mt-12 flex flex-col lgss:hidden">
+            <div className="">
+              <img src={logo} alt="logo" className="bg-cover" />
+            </div>
+            <div className="mt-12 w-full flex flex-col justify-between h-screen items-start font-manrope py-[30%]">
+              <div className="flex flex-col w-full">
+                <Link
+                  to="/overview"
+                  className={
+                    activebutton === 1
+                      ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                      : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+                  }
+                >
+                  <h4 className="">Overview</h4>
+                  <HiSquares2X2 />
+                </Link>
+                <Link
+                  to="/projects"
+                  className={
+                    activebutton === 2
+                      ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                      : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+                  }
+                >
+                  <h4 className="">Projects</h4>
+                  <HiClipboardDocumentCheck />
+                </Link>
+                <Link
+                  to="/settings"
+                  className={
+                    activebutton === 3
+                      ? "flex justify-between text-[20px] text-white items-center font-medium  rounded-full px-4 bg-primary w-full h-[60px]"
+                      : "flex justify-between text-[20px] text-[#6B7276] items-center font-medium  px-4  w-full h-[60px]"
+                  }
+                >
+                  <h4 className="">Settings</h4>
+                  <IoIosSettings />
+                </Link>
+              </div>
+              <button
+                onClick={() => navigate("/login")}
+                className="flex gap-5 text-[18px] text-[#B82323] items-center  font-medium pl-12 h-[40%] w-full"
+              >
+                Sign Out
+                <HiOutlineLogout />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Sidebar;
--- src/pages/webApp/Dashboard.jsx
+++ src/pages/webApp/Dashboard.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sidebar from "../../components/webApp/Sidebar";
 import Header from "../../components/webApp/Header";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Dashboard = () => {
   const [userName, setUserName] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
 
   useEffect(() => {
     // Fetch user name from the backend
@@ -21,10 +24,24 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="h-screen flex flex-row overflow-x-hidden">
-      <Sidebar />
-      <div className="w-4/5 h-full overflow-auto flex flex-col bg-white items-center font-lato justify-start">
+    <div className="lgss:h-screen flex flex-row overflow-x-hidden">
+      <Sidebar isOpen={isOpen} />
+
+      <div className="lgss:w-4/5 w-full h-full overflow-auto flex flex-col bg-white items-center font-lato justify-start">
         <div className="w-full">
+          <div className="lgss:hidden pt-5 px-[5%] flex w-full justify-end">
+            {isOpen ? (
+              <FaTimes
+                onClick={() => setIsOpen(false)}
+                className=" cursor-pointer text-secondary text-xl"
+              />
+            ) : (
+              <FaBars
+                onClick={() => setIsOpen(true)}
+                className=" cursor-pointer text-secondary text-xl"
+              />
+            )}
+          </div>
           <Header userName={userName} />
         </div>
       </div>
